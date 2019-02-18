c7d1e29323764c2c0f0c59ca1ad04514c713ae94
edward-p <micro.fedora@gmail.com>
2019-02-18 22:22:34 +0800

    Implement scheme && add scheme for blackarch

--- install.sh
+++ install.sh
@@ -4,21 +4,47 @@ theme_path=/usr/share/sddm/themes/dark-sddm-theme
 
 echo "Welcome in theme installer"
 
+logos=(
+    'Arch Linux'
+    'KDE neon'
+    'KDE plasma'
+    'Fedora'
+    'Blackarch'
+)
+schemes=(
+    'Default'
+    'Blackarch'
+)
+
 # Get favourite logo
 echo "Which is your favourite logo?"
-echo "(1) Arch Linux"
-echo "(2) KDE neon"
-echo "(3) KDE plasma"
-echo "(4) Fedora"
-# TODO -- update $max with the max logo option index
-max=4
+i=1
+for logo in "${logos[@]}"; do
+    echo "($i) $logo"
+    let "i=i+1"
+done
 
 echo -n "==> "
 read image
-if (( $image < 1 || $image > $max ))
+if (( $image < 1 || $image > ${#logos[@]} ))
 then
 	echo "Wrong selection. Aborting..."
 	exit 1
+fi
+
+# Get favourite scheme
+echo "Which is your favourite scheme?"
+i=1
+for scheme in "${schemes[@]}"; do
+    echo "($i) $scheme"
+    let "i=i+1"
+done
+echo -n "==> "
+read scheme
+if (( $scheme < 1 || $scheme > ${#schemes[@]} ))
+then
+    echo "Wrong selection. Aborting..."
+    exit 1
 fi
 
 # Install it
@@ -32,8 +58,17 @@ case $image in
 	2) sudo cp options/logo/neon.png $theme_path/resources/logo.png ;;
 	3) sudo cp options/logo/plasma.png $theme_path/resources/logo.png ;;
 	4) sudo cp options/logo/fedora.png $theme_path/resources/logo.png ;;
+    5) sudo cp options/logo/blackarch.png $theme_path/resources/logo.png ;;
 	*) echo "Unexpected error!"; exit 1 ;;
 esac
+
+# Install scheme
+case $scheme in
+    1) sudo cp options/scheme/default.qml $theme_path/Main.qml ;;
+    2) sudo cp options/scheme/blackarch.qml $theme_path/Main.qml ;;
+    *) echo "Unexpected error!"; exit 1 ;;
+esac
+
 
 echo "+-----------------+"
 echo "| THEME IS READY! |"
